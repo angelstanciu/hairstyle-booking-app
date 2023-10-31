6e50a540995f16fd82b810e5b663494f551592ce
Angel Stanciu <102602542+angelstanciu@users.noreply.github.com>
2023-10-31 17:42:08 +0200

    Initial commit

---
+++ src/main/java/com/angel/stanciu/hairstylebookingapp/HairstyleBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.angel.stanciu.hairstylebookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairstyleBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairstyleBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel/stanciu/hairstylebookingapp/HairstyleBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.stanciu.hairstylebookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairstyleBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
